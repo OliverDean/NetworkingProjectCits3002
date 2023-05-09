b4783bf35bf817e157f88da5ebaa8f3ab83f131a
Jamlons <theragingmonkey98@live.com>
2023-05-09 15:48:30 +0800

    working user + password system

--- TestMachine/TM.c
+++ TestMachine/TM.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <getopt.h>
 #include <string.h>
+#include <ctype.h>
 
 //TM WILL BE HOSTING ON http://192.168.220.118/
 
@@ -14,10 +15,53 @@ typedef struct curUser {
     int score; // Max of 30
 }curUser;
 
-int main(int argc, char* argv[]) {
-
-}
+curUser user;
 
 int login(char username[], char password[]) {
-    
+
+    char* line = NULL;
+    char* buf;
+    size_t linesize = 0;
+    ssize_t linelen;
+    int counter = 0;
+
+    /*
+    int fd = open("users.txt", O_RDONLY); // Open users.txt in read only mode
+    if (fd == -1) {
+        printf("Error number %d\n", errno);
+        perror("Program");
+    }
+    */
+
+    FILE *fp = fopen("users.txt", "r");
+
+    while ((linelen = getline(&line, &linesize, fp)) != -1) {
+        buf = strtok(line, " ");
+        if (!strcasecmp(buf, username)) { // If strings equal (ignoring case-sensitive for username)
+            strcpy(user.username, buf);
+            buf = strtok(NULL, " "); 
+            if (!strcasecmp(buf, password)) {
+                strcpy(user.password, buf);
+                printf("Correct Credentials\n");
+            }
+        }
+        else
+            continue;  
+    }   
+    //printf("%s", password);
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    char username[32];
+    char password[32];
+
+    printf("Please enter a username: ");
+    scanf("%s", username);
+
+    printf("\nPlease enter your password: ");
+    scanf("%s", password);
+ 
+    int temp = login(username, password);
+    return 0;
 }
