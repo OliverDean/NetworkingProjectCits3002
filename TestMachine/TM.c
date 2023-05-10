7d0450845aec2960c7a23044305022a38fbadb63
Jamlons <theragingmonkey98@live.com>
2023-05-10 10:48:51 +0800

    Added user data loading

--- TestMachine/TM.c
+++ TestMachine/TM.c
@@ -21,6 +21,44 @@ typedef struct curUser {
 
 curUser user;
 
+// Loads user data into structure
+void loadUser(char* buf) {
+    buf = strtok(NULL, ";");
+    user.userID = atoi(buf);
+    //printf("UserID: %d\n", user.userID);
+    for (int i = 0; i < 9; i++) {
+        buf = strtok(NULL, " ");
+        user.QB[i] = atoi(buf);
+        //printf("Question %d from QB %d\n", i+1, user.QB[i]);
+    }
+    buf = strtok(NULL, ";");
+    user.QB[9] = atoi(buf);
+    //printf("Question %d from QB %d\n", 10, user.QB[9]);
+    buf = strtok(NULL, ";");
+    user.questions = atoi(buf);
+    //printf("How many questions has user attempted: %d\n", user.questions);
+    for (int y = 0; y < 9; y++) {
+        buf = strtok(NULL, " ");
+        user.attempted[y] = atoi(buf);
+        //printf("User has attempted question %d, %d times\n", y+1, user.attempted[y]);
+    }
+    buf = strtok(NULL, ";");
+    user.attempted[9] = atoi(buf);
+    //printf("User has attempted question %d, %d times\n", 10, user.attempted[9]);
+    for (int x = 0; x < 9; x++) {
+        buf = strtok(NULL, " ");
+        user.score[x] = atoi(buf);
+        //printf("User has scored %d on question %d\n", user.score[x], x+1);
+    }
+    buf = strtok(NULL, ";");
+    user.score[9] = atoi(buf);
+    //printf("User has scored %d on question %d\n", user.score[9], 10);
+    buf = strtok(NULL, ";");
+    user.total_score = atoi(buf);
+    //printf("User has a total score of: %d\n", user.total_score);
+}
+
+// Logs user in, Returns 0 on success, 1 on password failure and returns 2 on login failure
 int login(char username[], char password[]) {
 
     char* line = NULL;
@@ -32,26 +70,29 @@ int login(char username[], char password[]) {
     FILE *fp = fopen("users.txt", "r");
 
     while ((linelen = getline(&line, &linesize, fp)) != -1) {
-        char *temp;
+        char temp[32];
         buf = strtok(line, " ");
         if (!strcmp(buf, "//")) // If comment line
             continue;
         if (!strcasecmp(buf, username)) { // If strings equal (ignoring case-sensitive for username)
-            strcpy(&temp, buf);
+            strcpy(temp, buf);
             buf = strtok(NULL, " "); 
             if (!strcasecmp(buf, password)) {
                 strcpy(user.password, buf);
-                strcpy(user.username, &temp);
-                printf("Correct Credentials\n");
+                strcpy(user.username, temp);
+                loadUser(buf);
+                return 0;
             }
             else {
                 printf("Incorrect Password\n");
+                return 1;
             }
         }
         else
             continue;  
-    }   
-    return 0;
+    }
+    fclose(fp);
+    return 2;
 }
 
 int main(int argc, char* argv[]) {
