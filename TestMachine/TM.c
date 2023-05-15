357f43a83d984cc9c7bc1614fbe005bfad1eb1c3
Jamlons <theragingmonkey98@live.com>
2023-05-15 14:09:09 +0800

    Condensed QuestionBank code (no more duplicates)

--- TestMachine/TM.c
+++ TestMachine/TM.c
@@ -1,9 +1,7 @@
 #include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <getopt.h>
 #include <string.h>
 #include <ctype.h>
 #include <sys/types.h>
@@ -13,13 +11,12 @@
 #include <arpa/inet.h>
 #include <sys/wait.h>
 #include <signal.h>
-#include <stdbool.h>
 #include <time.h>
 
 #define TMPORT "4125"
 #define PQBPORT "4126"
 #define CQBPORT "4127"
-#define BACKLOG 2
+#define BACKLOG 10
 
 typedef struct curUser
 {
@@ -41,7 +38,6 @@ curUser user;
 // Returns 0 on success
 int loadUser()
 {
-    bool flag = false;
     char *line = NULL;
     char *buf;
     size_t linesize = 0;
@@ -278,7 +274,6 @@ void generatenewfile()
             if (!strcmp(buf, user.password))
             {
                 buf = strtok(NULL, ";");
-                printf("password correct, buf is currently: %s\n", buf);
                 if (buf != NULL && strcmp(buf, "\n"))
                 { // If user has filename, re-write over it in the next step
                     printf("Found additional user cookie file: %s\n", buf);
@@ -303,6 +298,133 @@ void generatenewfile()
     fclose(fp);
     remove("users.txt");
     rename("temptemptemp", "users.txt");
+}
+
+void QuestionBanks(int QBsocket, int pipe[2])
+{
+    while (1)
+    {
+        char commandbuffer[3] = {0};
+        char *buf;
+        FILE *ft;
+        if (read(pipe[0], commandbuffer, 3) == -1) // Wait for instructions
+        {
+            perror("read");
+        }
+        if (!strcmp(commandbuffer, "GQ")) // Generate Questions
+        {
+            char questionIDbuffer[9] = {0};
+            char filename[9] = {0};
+            if (send(QBsocket, "GQ", 2, 0) == -1) // Send QB what it needs to prep for
+            {
+                perror("send");
+            }
+            if (recv(QBsocket, questionIDbuffer, sizeof(questionIDbuffer) - 1, MSG_WAITALL) == -1) // Wait for random questionID's
+            {
+                perror("recv");
+            }
+            questionIDbuffer[8] = '\0';                          // Make sure it is null terminated
+            if (read(pipe[0], filename, sizeof(filename)) == -1) // Send data to parent
+            {
+                perror("read");
+            }
+            user.user_filename = filename;
+            if (read(pipe[0], user.username, sizeof(user.username)) == -1) // Send data to parent
+            {
+                perror("read");
+            }
+            ft = fopen(user.user_filename, "a"); // Open file in append mode
+            if (ft == NULL)
+            {
+                perror("fopen");
+            }
+            buf = strtok(questionIDbuffer, ";"); // Grab the questionID
+            for (int i = 0; i < 4; i++)
+            {
+                fprintf(ft, "q;c;%s;---;\n", buf); // Add it to users cookie file
+                buf = strtok(NULL, ";");
+            }
+            fclose(ft);
+        }
+        else if (!strcmp(commandbuffer, "AN"))
+        { // If QB's need to answer questions
+            printf("meant to answer here..\n");
+            char *answer = "#include <stdio>\n\nint main(int argv, char *argv[]){\nprintf(\"Hello World!\");\nreturn 0;}";
+            int length = sizeof(answer);
+            int sendnumber = htonl(length);
+            char *isAnswer;
+            if (send(QBsocket, "AN", 2, 0) == -1) // Send QB what it needs to prep for
+            {
+                perror("send");
+            }
+            if (send(QBsocket, &sendnumber, sizeof(sendnumber), 0) == -1) // Send answer string size to QB
+            {
+                perror("send");
+            }
+            if (send(QBsocket, answer, sizeof(answer), 0) == -1) // Send answer to QB
+            {
+                perror("send");
+            }
+            if (send(QBsocket, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QuestionID to QB
+            {
+                perror("send");
+            }
+            if (recv(QBsocket, isAnswer, 1, 0) == -1) // Is answer right?
+            {
+                perror("recv");
+            }
+            if (!strcmp(isAnswer, "Y"))
+                printf("Answer is right\n");
+            else
+                printf("Answer is wrong\n");
+        }
+        else if (!strcmp(commandbuffer, "IQ"))
+        { // If QB's need to return the answer to a question (3 failed attempts)
+            printf("meant to get answer here...\n");
+            int length = 0;
+            char *answerBuf;
+            if (send(QBsocket, "IQ", 2, 0) == -1) // Send QB what it needs to prep for
+            {
+                perror("send");
+            }
+            if (send(QBsocket, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QB the questionID
+            {
+                perror("send");
+            }
+            if (recv(QBsocket, &length, sizeof(int), 0) == -1) // Get length of answer from QB
+            {
+                perror("recv");
+            }
+            if (recv(QBsocket, answerBuf, ntohl(length), MSG_WAITALL) == -1) // Get answer
+            {
+                perror("recv");
+            }
+            printf("%s\n", answerBuf);
+        }
+        else if (!strcmp(commandbuffer, "PQ"))
+        { // If QB's need to return the question from questionID
+            printf("meant to return the question here...\n");
+            int length = 0;
+            char *questionBuf;
+            if (send(QBsocket, "PQ", 2, 0) == -1) // Send QB what it needs to prep for
+            {
+                perror("send");
+            }
+            if (send(QBsocket, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QB the questionID
+            {
+                perror("send");
+            }
+            if (recv(QBsocket, &length, sizeof(int), 0) == -1) // Get length of answer from QB
+            {
+                perror("recv");
+            }
+            if (recv(QBsocket, questionBuf, ntohl(length), MSG_WAITALL) == -1) // Get answer
+            {
+                perror("recv");
+            }
+            printf("%s\n", questionBuf);
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -362,129 +484,7 @@ int main(int argc, char *argv[])
                 break;
             }
             printf("accepted connection from CQB\n");
-            while (1)
-            { // main accept() loop
-                char commandbuffer[3] = {0};
-                char *buf;
-                FILE *ft;
-                if (read(cqbpipe[0], commandbuffer, 3) == -1) // Wait for instructions
-                {
-                    perror("read");
-                }
-                if (!strcmp(commandbuffer, "GQ")) // Generate Questions
-                {
-                    char questionIDbuffer[9] = {0};
-                    char filename[9] = {0};
-                    if (send(newc_fd, "GQ", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (recv(newc_fd, questionIDbuffer, sizeof(questionIDbuffer) - 1, MSG_WAITALL) == -1) // Wait for random questionID's
-                    {
-                        perror("recv");
-                    }
-                    questionIDbuffer[8] = '\0'; // Make sure it is null terminated
-                    if (read(cqbpipe[0], filename, sizeof(filename)) == -1) // Send data to parent
-                    {
-                        perror("read");
-                    }
-                    user.user_filename = filename;
-                    if (read(cqbpipe[0], user.username, sizeof(user.username)) == -1) // Send data to parent
-                    {
-                        perror("read");
-                    }
-                    ft = fopen(user.user_filename, "a"); // Open file in append mode
-                    if (ft == NULL)
-                    {
-                        perror("fopen");
-                    }
-                    buf = strtok(questionIDbuffer, ";"); // Grab the questionID
-                    for (int i = 0; i < 4; i++)
-                    {
-                        fprintf(ft, "q;c;%s;---;\n", buf); // Add it to users cookie file
-                        buf = strtok(NULL, ";");
-                    }
-                    fclose(ft);
-                }
-                else if (!strcmp(commandbuffer, "AN"))
-                { // If QB's need to answer questions
-                    printf("meant to answer here..\n");
-                    char *answer = "#include <stdio>\n\nint main(int argv, char *argv[]){\nprintf(\"Hello World!\");\nreturn 0;}";
-                    int length = sizeof(answer);
-                    int sendnumber = htonl(length);
-                    char *isAnswer;
-                    if (send(newc_fd, "AN", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (send(newc_fd, &sendnumber, sizeof(sendnumber), 0) == -1) // Send answer string size to QB
-                    {
-                        perror("send");
-                    }
-                    if (send(newc_fd, answer, sizeof(answer), 0) == -1) // Send answer to QB
-                    {
-                        perror("send");
-                    }
-                    if (send(newc_fd, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QuestionID to QB
-                    {
-                        perror("send");
-                    }
-                    if (recv(newc_fd, isAnswer, 1, 0) == -1) // Is answer right?
-                    {
-                        perror("recv");
-                    }
-                    if (!strcmp(isAnswer, "Y"))
-                        printf("Answer is right\n");
-                    else
-                        printf("Answer is wrong\n");
-                }
-                else if (!strcmp(commandbuffer, "IQ"))
-                { // If QB's need to return the answer to a question (3 failed attempts)
-                    printf("meant to get answer here...\n");
-                    int length = 0;
-                    char *answerBuf;
-                    if (send(newc_fd, "IQ", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (send(newc_fd, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QB the questionID
-                    {
-                        perror("send");
-                    }
-                    if (recv(newc_fd, &length, sizeof(int), 0) == -1) // Get length of answer from QB
-                    {
-                        perror("recv");
-                    }
-                    if (recv(newc_fd, answerBuf, ntohl(length), MSG_WAITALL) == -1) // Get answer
-                    {
-                        perror("recv");
-                    }
-                    printf("%s\n", answerBuf);
-                }
-                else if (!strcmp(commandbuffer, "PQ"))
-                { // If QB's need to return the question from questionID
-                    printf("meant to return the question here...\n");
-                    int length = 0;
-                    char *questionBuf;
-                    if (send(newc_fd, "PQ", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (send(newc_fd, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QB the questionID
-                    {
-                        perror("send");
-                    }
-                    if (recv(newc_fd, &length, sizeof(int), 0) == -1) // Get length of answer from QB
-                    {
-                        perror("recv");
-                    }
-                    if (recv(newc_fd, questionBuf, ntohl(length), MSG_WAITALL) == -1) // Get answer
-                    {
-                        perror("recv");
-                    }
-                    printf("%s\n", questionBuf);
-                }
-            }
+            QuestionBanks(newc_fd, cqbpipe);
             close(newc_fd);
             close(cqbpipe[0]);
             close(cqbpipe[1]);
@@ -508,129 +508,7 @@ int main(int argc, char *argv[])
                 break;
             }
             printf("accepted connection from PQB\n");
-            while (1)
-            { // main accept() loop
-                char commandbuffer[3] = {0};
-                char questionIDbuffer[13] = {0};
-                char filename[9] = {0};
-                char *buf;
-                FILE *ft;
-                if (read(pqbpipe[0], commandbuffer, 3) == -1) // Wait for instructions
-                {
-                    perror("read");
-                }
-                if (!strcmp(commandbuffer, "GQ"))
-                { // If QB's need to generate questions
-                    if (send(newp_fd, "GQ", 2, 0) == -1)
-                    {
-                        perror("send");
-                        break;
-                    }
-                    sleep(0.01);
-                    if (recv(newp_fd, questionIDbuffer, sizeof(questionIDbuffer) - 1, 0) == -1)
-                    {
-                        perror("recv");
-                        break;
-                    }
-                    questionIDbuffer[12] = '\0';
-                    if (read(pqbpipe[0], filename, sizeof(filename)) == -1)
-                    {
-                        perror("read");
-                    }
-                    user.user_filename = filename;
-                    read(pqbpipe[0], user.username, sizeof(user.username));
-                    ft = fopen(user.user_filename, "a");
-                    if (ft == NULL)
-                    {
-                        perror("fopen");
-                    }
-                    buf = strtok(questionIDbuffer, ";");
-                    for (int i = 0; i < 6; i++)
-                    {
-                        fprintf(ft, "q;python;%s;---;\n", buf);
-                        buf = strtok(NULL, ";");
-                    }
-                    fclose(ft);
-                }
-                else if (!strcmp(commandbuffer, "AN"))
-                { // If QB's need to answer questions
-                    printf("meant to answer here..\n");
-                    char *answer = "#include <stdio>\n\nint main(int argv, char *argv[]){\nprintf(\"Hello World!\");\nreturn 0;}";
-                    int length = sizeof(answer);
-                    int sendnumber = htonl(length);
-                    char *isAnswer;
-                    if (send(newp_fd, "AN", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (send(newp_fd, &sendnumber, sizeof(sendnumber), 0) == -1) // Send answer string size to QB
-                    {
-                        perror("send");
-                    }
-                    if (send(newp_fd, answer, sizeof(answer), 0) == -1) // Send answer to QB
-                    {
-                        perror("send");
-                    }
-                    if (send(newp_fd, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QuestionID to QB
-                    {
-                        perror("send");
-                    }
-                    if (recv(newp_fd, isAnswer, 1, 0) == -1) // Is answer right?
-                    {
-                        perror("recv");
-                    }
-                    if (!strcmp(isAnswer, "Y"))
-                        printf("Answer is right\n");
-                    else
-                        printf("Answer is wrong\n");
-                }
-                else if (!strcmp(commandbuffer, "IQ"))
-                { // If QB's need to return the answer to a question (3 failed attempts)
-                    printf("meant to get answer here...\n");
-                    int length = 0;
-                    char *answerBuf;
-                    if (send(newp_fd, "IQ", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (send(newp_fd, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QB the questionID
-                    {
-                        perror("send");
-                    }
-                    if (recv(newp_fd, &length, sizeof(int), 0) == -1) // Get length of answer from QB
-                    {
-                        perror("recv");
-                    }
-                    if (recv(newp_fd, answerBuf, ntohl(length), MSG_WAITALL) == -1) // Get answer
-                    {
-                        perror("recv");
-                    }
-                    printf("%s\n", answerBuf);
-                }
-                else if (!strcmp(commandbuffer, "PQ"))
-                { // If QB's need to return the question from questionID
-                    printf("meant to return the question here...\n");
-                    int length = 0;
-                    char *questionBuf;
-                    if (send(newp_fd, "PQ", 2, 0) == -1) // Send QB what it needs to prep for
-                    {
-                        perror("send");
-                    }
-                    if (send(newp_fd, user.QuestionID[3], sizeof(user.QuestionID[3]), 0) == -1) // Send QB the questionID
-                    {
-                        perror("send");
-                    }
-                    if (recv(newp_fd, &length, sizeof(int), 0) == -1) // Get length of answer from QB
-                    {
-                        perror("recv");
-                    }
-                    if (recv(newp_fd, questionBuf, ntohl(length), MSG_WAITALL) == -1) // Get answer
-                    {
-                        perror("recv");
-                    }
-                    printf("%s\n", questionBuf);
-                }
-            }
+            QuestionBanks(newp_fd, pqbpipe);
             close(newp_fd);
             close(pqbpipe[0]);
             close(pqbpipe[1]);
@@ -718,7 +596,6 @@ int main(int argc, char *argv[])
                     close(newtm_fd);
                     close(cqbpipe[1]);
                     close(cqbpipe[0]);
-                    close(pqbpipe[1]);
                     close(pqbpipe[0]);
                     close(pqbpipe[1]);
                     break;
