10828d0fb5abfe81a55a74332aacb5bf325d25e4
Karla Ivkovic <karla-ivkvc@users.noreply.github.com>
2023-05-16 22:37:03 +0800

    password issue corrected

--- TestMachine/TM.c
+++ TestMachine/TM.c
@@ -452,7 +452,7 @@ void displaylogin(int newtm_fd) {
     fread(logintext, sizeof(char), length, loginfile); // Grab entire file
     int total_length = strlen(header) + strlen(logintext) + (sizeof(char) * 12);
     fullhttp = malloc(sizeof(char) * total_length);
-    printf("%s\n", logintext);
+    // printf("%s\n", logintext);
     snprintf(fullhttp, total_length, "%s%d\n\n%s", header, length, logintext);
     fclose(loginfile);
     write(newtm_fd, fullhttp, strlen(fullhttp));
@@ -463,12 +463,12 @@ void displaylogin(int newtm_fd) {
 void setUser(char *buffer, char username[32], char password[32])
 {
         char *pch;
-        pch = strtok(buffer, " ? = &");
+        pch = strtok(buffer, "?=&" );
         char *previous = pch;
         while (pch != NULL) 
         {
 
-            pch=strtok(NULL, " ? = &");
+            pch=strtok(NULL, " ? = & ");
             if (strcmp(previous, "username") == 0)
             {
                 strcpy(username, pch);
@@ -480,6 +480,15 @@ void setUser(char *buffer, char username[32], char password[32])
                 break;
             }
             previous = pch;
+        }
+        // here, until we reach the \0 bytes 
+        int count = 0;
+        for (int i =0; i < strlen(password); i++)
+        {
+            if (password[i] != '\n' || password[i] != '\r' || password[i] != '\0')
+            {
+                printf("\t%c\n", password[i]);
+            }
         }
 }
 
@@ -606,12 +615,23 @@ int main(int argc, char *argv[])
         char *test = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
         write(newtm_fd, test, strlen(test));
         */
+
+        memset(username, 0, sizeof(username));
+        memset(password, 0, sizeof(password));
+
+        //printf("1USERNAME: %s\n", username);
+        //printf("1PASSWORD: %s\n", password);
+            
         displaylogin(newtm_fd);
         char buffer[2500];
         recv(newtm_fd, buffer, sizeof(buffer),0);
         setUser(buffer, username, password);
-        printf("USERNAME: %s\n", username);
-        printf("PASSWORD: %s\n", password);
+        username[strcspn(username, "\n")] = '\0'; // Remove delimiters for string matching to work
+        username[strcspn(username, "\r")] = '\0';
+        password[strcspn(password, "\n")] = '\0';
+        password[strcspn(password, "\r")] = '\0';
+        //printf("2USERNAME: %s\n Length of USERNAME: %lu\n", username, strlen(username));
+        //printf("2PASSWORD: %s\n Length of PASSWORD: %lu\n", password, strlen(password));
         switch (fork())
         {
         case -1:
@@ -624,20 +644,6 @@ int main(int argc, char *argv[])
                 close(tm_fd);
                 char *returnvalue;
                 char httprequestvalue[2];
-                // memset(username, 0, sizeof(username));
-                // memset(password, 0, sizeof(password));
-                // if (send(newtm_fd, "Please enter a username: ", 25, 0) == -1)
-                //     perror("send");
-                // if (recv(newtm_fd, username, sizeof(username), 0) == -1)
-                //     perror("recv");
-                // if (send(newtm_fd, "Please enter a password: ", 25, 0) == -1)
-                //     perror("send");
-                // if (recv(newtm_fd, password, sizeof(password), 0) == -1)
-                //     perror("recv");
-                // username[strcspn(username, "\n")] = '\0'; // Remove delimiters for string matching to work
-                // username[strcspn(username, "\r")] = '\0';
-                // password[strcspn(password, "\n")] = '\0';
-                // password[strcspn(password, "\r")] = '\0';
 
                 int loginValue = login(username, password, &user.user_filename);
                 if (loginValue == -1) // Invalid Username (doesn't exist)
@@ -658,6 +664,7 @@ int main(int argc, char *argv[])
                 }
 
                 int loadvalue = loadUser();
+                printf("load value: %i\n", loadvalue);
                 if (loadvalue == -1) // If file failed to open
                 {
                     returnvalue = "NF";
